221dda6669fdc5e9c63442c67dd1cb2c330b62b5
DNightmare <sebi@ctc-gaming.com>
2021-03-06 14:21:45 +0100

    Initial commit

---
+++ NoPrologue/NoPrologue.cs
@@ -0,0 +1,38 @@
+﻿using BepInEx;
+using HarmonyLib;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NoPrologue
+{
+    [BepInPlugin(pluginGuid, pluginName, pluginVersion)]
+    public class NoPrologue : BaseUnityPlugin
+    {
+        public const string pluginGuid = "dsp.dnightmare.noprologue";
+        public const string pluginName = "No Prologue";
+        public const string pluginVersion = "1.0.0";
+
+        private bool cfgEnabled = true;
+
+        new internal static BepInEx.Logging.ManualLogSource Logger = BepInEx.Logging.Logger.CreateLogSource(pluginName);
+
+        void Awake()
+        {
+            cfgEnabled = Config.Bind<bool>("General", "enablePlugin", cfgEnabled, "enable/disable this plugin").Value;
+            if (cfgEnabled == true)
+            {
+                var harmony = new Harmony(pluginGuid);
+                harmony.PatchAll(typeof(NoPrologue));
+            }
+        }
+
+        [HarmonyPostfix, HarmonyPatch(typeof(UIGalaxySelect), "EnterGame")]
+        public static void EnterGamePostfix(ref GameDesc ___gameDesc)
+        {
+            DSPGame.StartGameSkipPrologue(___gameDesc);
+        }
+    }
+}
---
+++ NoPrologue/README.md
@@ -0,0 +1,4 @@
+## DNightmare's NoPrologue
+
+Tired of clicking those extra buttons when starting a new game to skip the prologue?
+This simple yet effective mod will do that for you.
